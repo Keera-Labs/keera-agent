d4d1b23c86027e54f0ac64eefe9a62baffdd0f07
Bedram Tamang <tmgbedu@gmail.com>
2026-06-05 12:35:55 -0700

    feat: claude \r in backend

--- resources/js/pages/Settings.tsx
+++
@@ -1,8 +0,0 @@
-import AppLayout from '../layouts/AppLayout'
-import SettingsView from '../layouts/SettingsView'
-
-export default function Settings() {
-    return <SettingsView />
-}
-
-Settings.layout = (page: React.ReactNode) => <AppLayout>{page}</AppLayout>
