a7996e1004a2be7f9de855d9b397e7759746182e
Bedram Tamang <tmgbedu@gmail.com>
2026-06-27 23:07:31 -0700

    chore: drop ineffective *.log reload exclude, default app_url to :4545

--- config/fastapi.py
+++ config/fastapi.py
@@ -5,12 +5,11 @@ from fastapi_startkit.environment import env
 
 @dataclasses.dataclass
 class FastAPIConfig:
-    app_url: str = dataclasses.field(default_factory=lambda: env("APP_URL", "http://127.0.0.1:8000"))
+    app_url: str = dataclasses.field(default_factory=lambda: env("APP_URL", "http://127.0.0.1:4545"))
     reload: bool = dataclasses.field(default_factory=lambda: env("APP_RELOAD", False))
     reload_dirs: list | None = None
     reload_excludes: list = dataclasses.field(
         default_factory=lambda: [
-            "*.log",
             "tests/*",
             "node_modules/*",
             "storage/*",
